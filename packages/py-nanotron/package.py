40d3c66e4f49b34b7d817a9f3933dfbcb9508902
Thomas Bouvier <contact@thomas-bouvier.io>
2025-03-23 18:22:37 +0100

    feat(py-nanotron): add variant datastates

--- packages/py-nanotron/package.py
+++ packages/py-nanotron/package.py
@@ -1,7 +1,8 @@
 from spack.package import *
 
 class PyNanotron(PythonPackage):
-    """Minimalistic large language model 3D-parallelism training."""
+    """Minimalistic large language model 3D-parallelism training.
+    """
 
     homepage = "https://github.com/korovod/nanotron"
     git = "https://github.com/korovod/nanotron.git"
@@ -13,18 +14,27 @@ class PyNanotron(PythonPackage):
     version("main", branch="main")
 
     variant("examples", default=True, description="Build with example scripts support")
+    variant("nanosets", default=True)
+    variant("datastates", default=False, description="Efficient asynchronous checkpointing using CUDA copy engines")
 
-    depends_on("python@3.6:3.11")
+    depends_on("python@3.9:3.11")
 
     depends_on("py-setuptools", type="build")
-    depends_on("py-torch@1.13.0:", type=("build", "run"))
+    depends_on("py-torch@2:", type=("build", "run"))
     depends_on("py-pyyaml", type=("build", "run"))
     depends_on("py-numpy@:2", type=("build", "run"))
     depends_on("py-packaging", type=("build", "run"))
     depends_on("py-safetensors", type=("build", "run"))
     depends_on("py-dacite", type=("build", "run"))
     depends_on("py-tqdm", type=("build", "run"))
+    depends_on("py-wandb", type=("build", "run"))
     depends_on("py-datasets", type=("build", "run"), when="@0.5:")
 
     depends_on("py-transformers", type=("build", "run"), when="+examples")
-    depends_on("py-flash-attn", type=("build", "run"), when="+examples")
+    depends_on("py-flash-attn@2.7:", type=("build", "run"), when="+examples")
+
+    depends_on("py-transformers", type=("build", "run"), when="+nanosets")
+    depends_on("py-numba", type=("build", "run"), when="+nanosets")
+    depends_on("py-datatrove", type=("build", "run"), when="+nanosets")
+
+    depends_on("py-datastates", type=("build", "run"), when="+datastates")
