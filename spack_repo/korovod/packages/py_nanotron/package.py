7e2e985f58b5c519b006f626ce5cc3eb1d58ca45
Thomas Bouvier <contact@thomas-bouvier.io>
2025-07-18 21:56:36 +0200

    feat(nanotron): update dependencies

--- spack_repo/korovod/packages/py_nanotron/package.py
+++ spack_repo/korovod/packages/py_nanotron/package.py
@@ -21,9 +21,9 @@ class PyNanotron(PythonPackage):
     depends_on("python@3.9:")
 
     depends_on("py-setuptools", type="build")
-    depends_on("py-torch@2:", type=("build", "run"))
+    depends_on("py-torch@2.5:", type=("build", "run"))
     depends_on("py-pyyaml", type=("build", "run"))
-    depends_on("py-numpy@:2", type=("build", "run"))
+    depends_on("py-numpy@2:", type=("build", "run"))
     depends_on("py-packaging", type=("build", "run"))
     depends_on("py-safetensors", type=("build", "run"))
     depends_on("py-dacite", type=("build", "run"))
@@ -31,7 +31,7 @@ class PyNanotron(PythonPackage):
     depends_on("py-wandb", type=("build", "run"))
     depends_on("py-datasets", type=("build", "run"), when="@0.5:")
     depends_on("py-transformers", type=("build", "run"))
-    depends_on("py-flash-attn@2.7:", type=("build", "run"))
+    #depends_on("py-flash-attn@2.7:", type=("build", "run"))
 
     # Nanosets
     depends_on("py-transformers", type=("build", "run"), when="+nanosets")
